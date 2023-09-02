5c8d7ee9dd41313ccfc74ad4b87c8f1edad2e8c6
AmiEklasMohin <mohin.gacc@gmail.com>
2023-09-03 00:33:24 +0600

    Codeforces 546A

---
+++ soldier_and_bananas.js
@@ -0,0 +1,12 @@
+function solveTheProblem(k, n, w) {
+    var totalPrice = 0;
+    for (var i=1; i<=w; i++) {
+        totalPrice += i * k;
+    }
+    var needToBorrow = totalPrice - n;
+    return Math.max(needToBorrow, 0);
+}
+var inputLine = readline().split(' ').map(x => parseInt(x));
+var k = inputLine[0], n = inputLine[1] , w = inputLine[2];
+var borrowFromFriend = solveTheProblem(k, n, w);
+print(borrowFromFriend);
