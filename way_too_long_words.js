cec21069e10ff407aa2c8a4e27c2d9ba3306d1f5
AmiEklasMohin <mohin.gacc@gmail.com>
2023-09-03 08:03:06 +0600

    Codeforces 71A

---
+++ way_too_long_words.js
@@ -0,0 +1,17 @@
+function solveTheProblem(inputWord) {
+    var outputWord = inputWord;
+    if (inputWord.length > 10) {
+        var wordLength = inputWord.length;
+        var firstLetter = inputWord.charAt(0);
+        var lastLetter = inputWord.charAt(wordLength - 1);
+        wordLength = wordLength - 2;
+        outputWord = firstLetter.concat(wordLength, lastLetter);
+    }
+    return outputWord;
+}
+var testCase = readline();
+for (var i = 1; i <= testCase; i++) {
+    var inputWord = readline();
+    var outputWord = solveTheProblem(inputWord);
+    print(outputWord);
+}
