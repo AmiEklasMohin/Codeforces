3c2a53039c4ff8539925ece1f3035b114b55999d
AmiEklasMohin <mohin.gacc@gmail.com>
2023-09-03 17:25:54 +0600

    Codeforces 231A

--- team.js
+++ team.js
@@ -1,9 +1,9 @@
 function solveTheProblem(numberOfProblems) {
     var count = 0;
-    for(var i = 1; i <= numberOfProblems; i++) {
+    for (var i = 1; i <= numberOfProblems; i++) {
         var isSure = readline().split(' ').map(x => parseInt(x));
         var sureAboutSolution = isSure[0] + isSure[1] + isSure[2];
-        if(sureAboutSolution >= 2) {
+        if (sureAboutSolution >= 2) {
             count++;
         }
     }
