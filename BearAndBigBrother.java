2f16711246ba8b80d4638d97210691db211f6f1d
AmiEklasMohin <mohin.gacc@gmail.com>
2023-09-04 22:29:07 +0600

    Codeforces 791A

--- BearAndBigBrother.java
+++ BearAndBigBrother.java
@@ -1,8 +1,7 @@
 import java.util.Scanner;
 
 public class BearAndBigBrother {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
         int weightOfLimak = sc.nextInt();
         int weightOfBob = sc.nextInt();
