5d0aa99a4a57d13ebe5e0a9c4255c2083f3e7a26
AmiEklasMohin <mohin.gacc@gmail.com>
2023-09-04 21:56:19 +0600

    Codeforces 1030A

---
+++ InSearchOfEasyProblem.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class InSearchOfEasyProblem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n, k, j = 0;
+        n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            k = sc.nextInt();
+            if (k == 1) {
+                j++;  
+            }
+        }
+        if(j > 0) {
+            System.out.println("HARD");
+        }
+        else {
+            System.out.println("EASY");
+        }  
+        sc.close();
+    }
+}
