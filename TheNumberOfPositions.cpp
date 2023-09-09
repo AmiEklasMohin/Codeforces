76f872ad15f416badb27d5b2ce8be49c7f59aeff
AmiEklasMohin <mohin.gacc@gmail.com>
2023-09-09 14:51:40 +0600

    Codeforces 124A

--- TheNumberOfPositions.cpp
+++ TheNumberOfPositions.cpp
@@ -1,18 +1,11 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <cmath>
+#include <algorithm>
 using namespace std;
-void solveTheProblem() {
+int main() {
     int n, a, b, k;
     cin >> n >> a >> b;
     k = max((n - a - b - 1), 0);
     cout<<(n - a - k)<<endl;
-}
-int32_t main(int argc, const char *argv[]) {
-    ios_base::sync_with_stdio(false);cin.tie(nullptr);
-    cout.tie(nullptr);
-    #ifndef ONLINE_JUDGE
-        freopen("input.txt","r",stdin);
-        freopen("output.txt","w",stdout);
-    #endif
-    solveTheProblem();
     return 0;
 }
