fb9be398c5186b6850fc28f463c4a031e243f92b
EklasMohin <mohin@Eklas.mylinux.com>
2023-10-04 20:54:08 +0600

    Codeforces 124A

--- TheNumberOfPositions.cpp
+++ TheNumberOfPositions.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
-#include <cmath>
-#include <algorithm>
 using namespace std;
+
 int main() {
-    int n, a, b, k;
+    int n, a, b;
     cin >> n >> a >> b;
-    k = max((n - a - b - 1), 0);
-    cout<<(n - a - k)<<endl;
+    int k = max((n - a - b - 1), 0);
+    cout << n - a - k << endl;
     return 0;
 }
