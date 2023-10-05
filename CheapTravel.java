f57e5f86f96180b0750c7d5cc8ccd371dff10fd7
EklasMohin <mohin@Eklas.mylinux.com>
2023-10-05 20:05:46 +0600

    Codeforces 466A

--- CheapTravel.java
+++ CheapTravel.java
@@ -2,25 +2,27 @@ import java.util.Scanner;
 import java.lang.Math;
 
 public class CheapTravel {
-    static int minimumSumOne(int n, int m, int a, int b) {
+    public static int minimumSumOne(int n, int m, int a, int b) {
         int minimumRubles = a * n;
         return minimumRubles;
     }
-    static int minimumSumTwo(int n, int m, int a, int b) {
+
+    public static int minimumSumTwo(int n, int m, int a, int b) {
         int k = n / m;
+        int p = (n - (k * m)) * a;
+        p = Math.min(p, b);
         int minimumRubles = k * b;
-        int x = (n - (k * m)) * a;
-        x = Math.min(x, b);
-        minimumRubles += x;
+        minimumRubles += p;
         return minimumRubles;
     }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int m = scanner.nextInt();
         int a = scanner.nextInt();
         int b = scanner.nextInt();
-        int minimumRubles =  minimumSumOne(n, m, a, b);
+        int minimumRubles = minimumSumOne(n, m, a, b);
         minimumRubles = Math.min(minimumSumTwo(n, m, a, b), minimumRubles);
         System.out.println(minimumRubles);
         scanner.close();
