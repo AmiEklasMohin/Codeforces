285e2ffc1646a408a0068b7c042b06a61d0c9b68
EklasMohin <mohin@Eklas.mylinux.com>
2023-10-05 23:50:47 +0600

    Codeforces 231A

--- team.js
+++ team.js
@@ -1,14 +1,15 @@
-function solveTheProblem(numberOfProblems) {
-    var count = 0;
+function writeSolution(numberOfProblems) {
+    var solveCount = 0;
     for (var i = 1; i <= numberOfProblems; i++) {
-        var isSure = readline().split(' ').map(x => parseInt(x));
+        var isSure = readline().split(' ').map(x => Number.parseInt(x));
         var sureAboutSolution = isSure[0] + isSure[1] + isSure[2];
         if (sureAboutSolution >= 2) {
-            count++;
+            solveCount += 1;
         }
     }
-    return count;
+    return solveCount;
 }
-var numberOfProblems = readline();
-var solutionImpelmented = solveTheProblem(numberOfProblems);
+
+var numberOfProblems = Number.parseInt(readline());
+var solutionImpelmented = writeSolution(numberOfProblems);
 print(solutionImpelmented);
