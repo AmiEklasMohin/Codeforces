ed77c7e01ba918fab7bc27b6716b08a44ecbe2d6
EklasMohin <mohin@Eklas.mylinux.com>
2023-10-06 15:22:53 +0600

    Codeforces 785A

--- AntonAndPolyhedrons.java
+++ AntonAndPolyhedrons.java
@@ -1,25 +1,23 @@
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class AntonAndPolyhedrons {
-    public static void main(String []args) {
-        Scanner sc = new Scanner (System.in);
-        int n, m = 0;
-        n = sc.nextInt();
-        for (int i = 1; i <= n; i++) {
-            String name = sc.next();
-            if ("Tetrahedron".equals(name)) {
-                m = m + 4;
-            } else if ("Cube".equals(name)) {
-                m = m + 6;
-            } else if ("Octahedron".equals(name)) {
-                m = m + 8;
-            } else if ("Dodecahedron".equals(name)) {
-                m = m + 12;
-            } else if ("Icosahedron".equals(name)) {
-                m = m + 20;
-            }
+    public static void main(String[] args) {
+        HashMap<String, Integer> polyhedronsList = new HashMap<>();
+        polyhedronsList.put("Tetrahedron", 4);
+        polyhedronsList.put("Cube", 6);
+        polyhedronsList.put("Octahedron", 8);
+        polyhedronsList.put("Dodecahedron", 12);
+        polyhedronsList.put("Icosahedron", 20);
+
+        int numberOfPolyhedrons, totalFaces = 0;
+        Scanner scanner = new Scanner(System.in);
+        numberOfPolyhedrons = scanner.nextInt();
+        for (int i = 1; i <= numberOfPolyhedrons; i++) {
+            String nameOfPolyhedron = scanner.next();
+            totalFaces += polyhedronsList.get(nameOfPolyhedron);
         }
-        System.out.println(m);
-        sc.close();
+        System.out.println(totalFaces);
+        scanner.close();
     }
 }
