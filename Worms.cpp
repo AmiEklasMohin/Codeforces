b11e1dca94d017a9ed1f1c4606bad5740624037d
EklasMohin <mohin@Eklas.mylinux.com>
2023-10-08 22:58:38 +0600

    Codeforces 474B

--- Worms.cpp
+++ Worms.cpp
@@ -4,21 +4,25 @@
 using namespace std;
 
 int main() {
-    int numberOfPiles, numberOfWorms[100005];
-    int numberOfJuicyWorms, labelOfTheJuicyWorms;
-    vector<long long>cumulativeSum;
+    int numberOfPiles;
     cin >> numberOfPiles;
+    vector<int> numberOfWorms(100005);
+    int numberOfJuicyWorms, labelOfTheJuicyWorms;
+    vector<long long> cumulativeSum;
     for (int i = 0; i < numberOfPiles; i++) {
         cin >> numberOfWorms[i];
     }
-    cumulativeSum.push_back(numberOfWorms[0]);
+    cumulativeSum.push_back((long long) numberOfWorms[0]);
     for (int i = 1; i < numberOfPiles; i += 1) {
         cumulativeSum.push_back(cumulativeSum[i - 1] + numberOfWorms[i]);
     }
     cin >> numberOfJuicyWorms;
     for (int i = 1; i <= numberOfJuicyWorms; i += 1) {
         cin >> labelOfTheJuicyWorms;
-        int pileNumber = lower_bound(cumulativeSum.begin(),cumulativeSum.end(), labelOfTheJuicyWorms) - cumulativeSum.begin();
+        int pileNumber = lower_bound(cumulativeSum.begin(), cumulativeSum.end(), (long long) labelOfTheJuicyWorms) - cumulativeSum.begin();
+        if (pileNumber < 0) {
+            pileNumber = -pileNumber - 1;
+        }
         pileNumber += 1;
         cout << pileNumber << endl;
     }
