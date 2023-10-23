7ce1be288ce589668d493e30580ac44d72e93ee3
EklasMohin <mohin@Eklas.mylinux.com>
2023-10-23 12:58:23 +0600

    Codeforces 231A

--- team.cpp
+++ team.cpp
@@ -1,57 +1,34 @@
-/***************
- ***__MOHIN__***
- ***************/
+/* author: mohin */
+
 #include <iostream>
-#include <cstdio>
-#include <cstring>
-#include <cmath>
-#include <sstream>
-#include <fstream>
-#include <cctype>
-#include <cstdlib>
-#include <string>
-#include <iomanip>
-#include <utility>
-#include <numeric>
-#include <set>
-#include <queue>
-#include <stack>
-#include <list>
-#include <vector>
-#include <map>
-#include <unordered_set>
-#include <unordered_map>
-#include <algorithm>
-#include <iterator>
-#define pi (2*acos(0))
-#define md 1000000007
-#define mx 1000000
+using namespace std;
 
 class Solution {
 public:
-    void solve() {
-        int number_of_problems, solve_count{0};
+    int solveCount() {
+        int num_of_problems, solve_count{0};
         bool sure[3];
-        std::cin >> number_of_problems;
-        for (int i = 1; i <= number_of_problems; i += 1) {
-            std::cin >> sure[0] >> sure[1] >> sure[2];
-            int total = sure[0] + sure[1] + sure[2];
-            if (total >= 2) {
+        cin >> num_of_problems;
+        for (int i = 1; i <= num_of_problems; ++i) {
+            cin >> sure[0] >> sure[1] >> sure[2];
+            int x = sure[0] + sure[1] + sure[2];
+            if (x >= 2) {
                 solve_count += 1;
             }
         }
-        std::cout << solve_count << std::endl;
+        return solve_count;
     }
 };
 
-int32_t main(int argc, const char *argv[]) {
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(nullptr);std::cout.tie(nullptr);
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);cout.tie(nullptr);
     #ifndef ONLINE_JUDGE
         freopen("input.txt", "r", stdin);
         freopen("output.txt", "w", stdout);
     #endif
-    Solution team;
-    team.solve();
+    Solution ans;
+    int problems_implemented = ans.solveCount();
+    cout << problems_implemented << endl;
     return 0;
 }
