7563c9ab5f9ffb479ab801f6571df233bd5387e3
EklasMohin <mohin@Eklas.mylinux.com>
2023-12-28 10:34:07 +0600

    Codeforces 1573D

--- Node.js/eraser.js
+++ Node.js/eraser.js
@@ -21,7 +21,25 @@ function readline() {
     return inputString[currentLine++];
 }
 
+function minOperation(n, k, str) {
+    let op = 0;
+    for (let i = 0; i < n; ++i) {
+        if (str.charAt(i) === 'B') {
+            op += 1;
+            i = i + k - 1;
+        }
+    }
+    return op;
+}
+
 function main() {
     let testCase = parseInt(readline());
-    
+    for (let i = 1; i <= testCase; ++i) {
+        let num = readline().split(' ').map(x => parseInt(x));
+        let str = readline();
+        let n = num[0];
+        let k = num[1];
+        let minOp = minOperation(n, k, str);
+        console.log(minOp);
+    }
 }
