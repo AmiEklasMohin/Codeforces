a67cbef1bdb6eface75386131cd3c5de174bb0ca
EklasMohin <mohin@Eklas.mylinux.com>
2024-01-02 15:01:46 +0600

    Js Template

---
+++ Node.js/template.js
@@ -0,0 +1,42 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    main();    
+});
+
+function readline() {
+    return inputString[currentLine++];
+}
+
+function gcd(a, b) {
+    while (b !== 0) {
+        let temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+function isCoprime(a, b) {
+    return (gcd(a, b) === 1);
+}
+
+function main() {
+    let testCase = parseInt(readline());
+    for (let i = 1; i <= testCase; ++i) {
+        
+    }
+}
